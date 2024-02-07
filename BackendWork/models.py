56be5fb969fe390af1ffdbca85a52855e973b147
NathanHuang86 <78175507+NathanHuang86@users.noreply.github.com>
2024-02-07 15:28:15 -0600

    User Update
    Updating the model for modUser

--- BackendWork/models.py
+++ BackendWork/models.py
@@ -1,6 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
 class modUser(AbstractUser):
     pass
+    firstName = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    lastName = models.CharField(max_length=20)
+    address = models.CharField(max_length=72)
+    phoneNumber = models.CharField(max_length=10)
+    registrationDate = models.CharField(max_length=10)
