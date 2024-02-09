bb659f7c4584f97a7c74a2b9cc7dc43163e699d2
Matt Meyer <mdmeyer@uwm.edu>
2024-02-09 13:33:37 -0600

    registered models in admin.py so they are visible on Django admin site

--- BackendWork/admin.py
+++ BackendWork/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import modUserCreationForm, modUserChangeForm
-from .models import modUser
+from .models import modUser, User, Address, Storefront, CustomerReviews, StoreReviews, Product, Category, SubCategory, \
+    ProductVideos, ProductImages, ProductQuestions, ProductReviews, Invoice, LineItem, DisputeTicket
 
 
 class modUserAdmin(UserAdmin):
@@ -13,4 +14,20 @@ class modUserAdmin(UserAdmin):
     list_display = ["email", "username", "password", "firstName", "lastName", "phoneNumber", "registrationDate"]
 
 
+# Register models here
 admin.site.register(modUser, modUserAdmin)
+admin.site.register(User)
+admin.site.register(Address)
+admin.site.register(Storefront)
+admin.site.register(CustomerReviews)
+admin.site.register(StoreReviews)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(ProductVideos)
+admin.site.register(ProductImages)
+admin.site.register(ProductQuestions)
+admin.site.register(ProductReviews)
+admin.site.register(Invoice)
+admin.site.register(LineItem)
+admin.site.register(DisputeTicket)
