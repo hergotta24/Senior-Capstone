ab4172e3a8e82f25101aa873f9af69ba25e8e10e
Nick <luedtken@uwm.edu>
2024-03-05 19:25:08 -0600

    Still need to mess with calling the view correctly, but getting there

--- BackendWork/unitTests/testProductDeletion.py
+++ BackendWork/unitTests/testProductDeletion.py
@@ -6,13 +6,37 @@ from django.test import Client
 class TestProductDeletion(TestCase):
     def setUp(self):
         self.client = Client()
-
-        #  self.client.storefront = Storefront()
-        #  self.product = Product(price=10.02, weight=15.1, name="testProduct", length=10, width=10, height=10, soldByStoreId=5)
-        #  self.product.save()
+        self.user = User(username="testuser", email="email@gmail.com", password="<PASSWORD>")
+        self.user.save()
+        self.client.storefront = Storefront(owner=self.user, name="testname", description="testdescription")
+        self.client.storefront.save()
+        self.product1 = Product(price=10.02, weight=15.1, name="testProduct", length=10, width=10, height=10,
+                               soldByStoreId=self.client.storefront)
+        self.product1.save()
+        self.product2 = Product(price=10.02, weight=15.1, name="testProduct2", length=10, width=10, height=10,
+                                soldByStoreId=self.client.storefront)
+        self.product2.save()
 
     def testProductDeletion(self):
-        product_id = self.product.productId
-        print(product_id)
+        product_id = self.product1.productId
         Product.objects.filter(productId=product_id).delete()
+        # self.client.post("delete/", )
+        # I need to figure out what to pass into the post, I know it will work if I find the right thing because
+        # The comment before this works, and that's all that the view calls
         self.assertFalse(Product.objects.filter(productId=product_id).exists())
+
+    def testMultipleItems(self):    # to make sure it isn't deleting everything
+        product_id1 = self.product1.productId
+        product_id2 = self.product2.productId
+        Product.objects.filter(productId=product_id1).delete()
+        self.assertTrue(Product.objects.filter(productId=product_id2).exists())
+
+    def testMultipleDeletions(self):
+        product_id1 = self.product1.productId
+        product_id2 = self.product2.productId
+        Product.objects.filter(productId=product_id1).delete()
+        Product.objects.filter(productId=product_id2).delete()
+        self.assertFalse(Product.objects.filter(productId=product_id1).exists() and
+                         Product.objects.filter(productId=product_id2).exists())
+
+
